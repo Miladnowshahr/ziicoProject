2b33f4a371bfa7ae323bff8dc22a9f7207862664
Milad Nowshahr <miladnowshahr@yahoo.com>
2020-08-14 17:52:58 +0430

    107 completed .

---
+++ ECommerce/Ecommerce/ECommerce.Presentation.Web/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presentation.Web.Controllers
+{
+    public class AccountController:Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignIn(string email,string password, bool isRemmeberMe)
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(string email, string password,string firstName,string lastName)
+        { 
+            return View();
+        }
+ 
+    }
+}
